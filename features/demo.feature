2f3b6a8d61700fb6c4231ce59725f7fd370f13f8
rudiz <rudizbb>
2018-02-20 18:20:54 +0200

    Homework3_Login_test

--- features/demo.feature
+++ features/demo.feature
@@ -1,9 +1,9 @@
 Feature: Demo Feature
 
-    Scenario: Logged in Successfully
+    Scenario: Successful Login
         Given I Am On Appimation Home Page
         When I Click Login
-        And I Enter demo@demo.com in Login Email Field
-        And I Enter parole112 in Login Password Field
+        And I Enter demo@demo.com In Login Email
+        And I Enter parole112 In Login Password
         Then I Click on Login Button
-        Then I Have Successfully Logged in
+        Then I Am Successfully Logged In 
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,26 +1,29 @@
-When(/^I Am On Appimation Home Page/) do
+When(/^I am on Appimation home page/) do
   visit('/')
   find(:id, 'start_button').visible?
   find(:id, 'login-b').visible?
   find(:id, 'signup-b').visible?
 end
 
-Then(/^I Click Login/) do
+Then(/^I click Login/) do
   find(:id, 'login-b').click
 end
 
-Then(/^I Enter (.*) in Login Email Field/) do |email|
+Then(/^I enter (.*) in Login email/) do |email|
   find(:xpath, '//div[@id = "login"]/descendant::input[@name = "login"]').send_keys email
 end
 
-Then(/^I Enter (.*) in Login Password Field/) do |password|
+Then(/^I enter (.*) in Login password/) do |password|
   find(:xpath, '//div[@id = "login"]/descendant::input[@name = "password"]').send_keys password
 end
 
-Then(/^I Click on Login Button/) do
+
+Then(/^I Login/) do
   find(:xpath, '//*[@id="login"]/form/button').click
+
 end
 
-Then(/^I Have Successfully Logged in/) do
+Then(/^I am successfully logged in/) do
   find(:id, 'logoutButton').visible?
+
 end
